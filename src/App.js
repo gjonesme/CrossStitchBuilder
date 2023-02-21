ad25b2289f8386b9cbc44a52da294f9b1293064b
Gary Jones <garyjones@Garys-Air.attlocal.net>
2023-02-20 22:19:14 -0500

    updating overall css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CSAppBar from "./Components/CSAppBar";
+import CrossStitchBuilder from "./Components/CrossStitchBuilder";
 
 function App() {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleBack = () => {
+    setActiveStep((activeStep) => activeStep - 1);
+  };
+
+  const handleNext = () => {
+    setActiveStep((activeStep) => activeStep + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CSAppBar
+        handleStep={handleStep}
+        handleReset={handleReset}
+        handleBack={handleBack}
+        handleNext={handleNext}
+        activeStep={activeStep}
+      />
+      <CrossStitchBuilder activeStep={activeStep} />
     </div>
   );
 }
